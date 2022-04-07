325db202c96bfd0d31a6f9709af933c445f419cb
gsq <1811400940@qq.com>
2022-04-08 00:07:40 +0800

    Lexer fixed

--- Lexer/Lexer.cpp
+++ Lexer/Lexer.cpp
@@ -70,28 +70,28 @@ void initMap() {
     kMap.push_back(make_pair("FALSE", FALSE));
     kMap.push_back(make_pair("IS", IS));
     kMap.push_back(make_pair("NOT", NOT));
-    kMap.push_back(make_pair("NULL_", NULL_));
+    kMap.push_back(make_pair("NULL", NULL_));
 
 
     oMap.clear();
-    oMap.push_back(make_pair("EQUAL", EQUAL));
-    oMap.push_back(make_pair("GRT_THAN", GRT_THAN));
-    oMap.push_back(make_pair("LES_THAN", LES_THAN));
-    oMap.push_back(make_pair("GRT_EQUAL", GRT_EQUAL));
-    oMap.push_back(make_pair("LES_EQUAL", LES_EQUAL));
-    oMap.push_back(make_pair("NOT_EQUAL", NOT_EQUAL));
-    oMap.push_back(make_pair("NOT_EQUAL_", NOT_EQUAL_));
-    oMap.push_back(make_pair("AND", AND));
-    oMap.push_back(make_pair("AND_", AND_));
-    oMap.push_back(make_pair("OR", OR));
-    oMap.push_back(make_pair("OR_", OR_));
+    oMap.push_back(make_pair("=", EQUAL));
+    oMap.push_back(make_pair(">", GRT_THAN));
+    oMap.push_back(make_pair("<", LES_THAN));
+    oMap.push_back(make_pair(">=", GRT_EQUAL));
+    oMap.push_back(make_pair("<=", LES_EQUAL));
+    oMap.push_back(make_pair("!=", NOT_EQUAL));
+    oMap.push_back(make_pair("<=>", NOT_EQUAL_));
+    oMap.push_back(make_pair("&&", AND));
+    oMap.push_back(make_pair("AND", AND_));
+    oMap.push_back(make_pair("||", OR));
+    oMap.push_back(make_pair("OR", OR_));
     oMap.push_back(make_pair("XOR", XOR));
-    oMap.push_back(make_pair("POINT", POINT));
+    oMap.push_back(make_pair(".", POINT));
 
     sMap.clear();
-    sMap.push_back(make_pair("LEFT_BRA", LEFT_BRA));
-    sMap.push_back(make_pair("RIGHT_BRA", RIGHT_BRA));
-    sMap.push_back(make_pair("COMMA", COMMA));
+    sMap.push_back(make_pair("(", LEFT_BRA));
+    sMap.push_back(make_pair(")", RIGHT_BRA));
+    sMap.push_back(make_pair(",", COMMA));
 
     MMap.clear();
     MMap.push_back(make_pair("STR", STR));
@@ -143,25 +143,29 @@ int seekKey(string &wor) {
     const char * word = wor.c_str();
     for (int i = 0; i < int(kMap.size()); i++) {
         if (strcmp(word, kMap[i].first) == 0) {
+            ////cout <<kMap[i].second<<endl; 
             return kMap[i].second;
         }
     }
     for (int i = 0; i < int(oMap.size()); i++) {
         if (strcmp(word, oMap[i].first) == 0) {
+            ////cout <<oMap[i].second<<endl; 
             return oMap[i].second;
         }
     }
     for (int i = 0; i < int(sMap.size()); i++) {
         if (strcmp(word, sMap[i].first) == 0) {
+            ////cout <<sMap[i].second<<endl; 
             return sMap[i].second;
         }
     }
     for (int i = 0; i < int(MMap.size()); i++) {
         if (strcmp(word, MMap[i].first) == 0) {
+            ////cout <<MMap[i].second<<endl; 
             return MMap[i].second;
         }
     }
-    return IDN;
+    return OTHER;
 }
 
 
@@ -188,8 +192,16 @@ void skipBlank() {
 }
 void dealBegin(string &name, TokenType &type){
     if(isdigit(*iter)) state = State::IN_INT; //!
-    else if(isalpha(*iter) || *iter=='_') { cout<<"INTO IDN"<<endl; state = State::IN_IDN;}
+    else if(isalpha(*iter) || *iter=='_') { 
+        ////cout<<"INTO IDN"<<endl; 
+        state = State::IN_IDN;
+    }
     else if(*iter==',' || *iter=='(' || *iter==')') state = State::IN_SE;
+    else if(*iter=='\'' || *iter=='\"') {
+        state = State::IN_STR;
+        name+=*iter;
+        iter++;
+    }
     else state = State::IN_OP;
 }
 void dealInt(string &name, TokenType &type){
@@ -219,13 +231,17 @@ void dealIDN(string &name, TokenType &type){
     if(isalnum(*iter)) {
         name+=*iter;
         ////cout << typeid(name).name() << endl;
-        cout << name << endl;
-        cout << seekKey(name) << endl;
+        ////cout << name << endl;
+        ////cout << seekKey(name) << endl;
         iter++;
         ////cout << *iter << endl; //cout << typeid(*iter).name() << endl;
-    } else if((seekKey(name) != IDN) && seekKey(name)<30) {
+    } else if((seekKey(name) != OTHER) && seekKey(name)<30) {
         state = State::IN_KW;
-        cout << "INTO KW1" << endl;
+        ////cout << "INTO KW1" << endl;
+    }else if((seekKey(name) >50) && seekKey(name)<70) {
+        state = State::IN_OP;
+        iter--;
+        ////cout << "INTO OP-AND1" << endl;
     } else {
         type = TokenType::IDN;
         state = State::END;
@@ -249,6 +265,11 @@ void dealOp(string &name, TokenType &type){
         iter++;
         type = TokenType::OP;
         state = State::END;
+    } else if(*iter=='.') {
+        name+=*iter;
+        iter++;
+        type = TokenType::OP;
+        state = State::END;
     } else if(*iter=='!' || *iter=='&' || *iter=='|' || *iter=='<' || *iter=='>') {
         name+=*iter;
         const char lastOp = *iter;
@@ -267,25 +288,43 @@ void dealOp(string &name, TokenType &type){
             type = TokenType::OP;
             state = State::END;
         }
+    } else if(*iter =='D') {
+        iter++;
+        ////cout << *iter << endl;
+        type = TokenType::OP;
+        state = State::END;
+    } else if(*iter =='R') {
+        iter++;
+        type = TokenType::OP;
+        state = State::END;
     }
     else { type = TokenType::OTHER; state = State::END; }
 }
 
+
 void dealStr(string &name, TokenType &type){
     //TODO
+    if((*iter=='\'')||(*iter=='\"')){
+        type = TokenType::STR;
+        state = State::END;
+    }
+    name+=*iter;
+    iter++;
 }
+
 TokenNode dealEnd(string &name, const TokenType &type){
     TokenNode EndToken;
     //!EndToken = new TokenNode();
-    cout << name << "***"<< type << "***" << line << endl;
+    ////cout << name << "***"<< type << "***" << line << endl;
     
     char *value;
     if(type==KW) sprintf(value, "%d", seekKey(name));
     ////cout << value << endl;
+    
     char *name_;
     sprintf(name_, "%s", name.c_str());
     EndToken = TokenNode(name_, type, value, line);
-    cout << "return EndToken" <<endl;
+    ////cout << "return EndToken" <<endl;
     return EndToken;
 }
 
@@ -317,13 +356,14 @@ TokenNode getNextToken() {
     //!token = new TokenNode();
 
     skipBlank();
-    cout << "After skipping Blank" << endl;
-    //cout << *iter << endl;
+    ////cout << "After skipping Blank" << endl;
+    
+    ////cout << *iter << endl;
     while((iter!=code.cend() && *iter!=EOF) || state==State::END) {
         switch(state) {
         case State::BEGIN:
             dealBegin(name,type);
-            cout << "Begin" <<endl;
+            ////cout << "Begin" <<endl;
             break;
         case State::IN_SE:
             dealSe(name,type);
@@ -339,9 +379,10 @@ TokenNode getNextToken() {
             break;
         case State::IN_KW:
             dealKw(name,type);
-            cout << "INTO KW" << endl;
+            ////cout << "INTO KW" << endl;
             break;
         case State::IN_OP:
+            ////cout << "AND / OR / XOR" <<endl;
             dealOp(name,type);
             break;
         case State::IN_STR:
@@ -350,22 +391,24 @@ TokenNode getNextToken() {
         case State::END:
             token = dealEnd(name,type);
             ////auto token = dealEnd(name,type);
-            cout<<"END"<<endl;
-            cout<<token.name<<endl;
-            cout<<token.value<<endl;
-            cout<<token.type<<endl;
-            cout<<token.line<<endl;
-            cout<<token.index<<endl;
+            strcpy(token.value, token.name); 
+            ////cout<<"END"<<endl;
+            ////cout<<token.name<<endl;
+            ////cout<<token.value<<endl;
+            ////cout<<token.type<<endl;
+            ////cout<<token.line<<endl;
+            ////cout<<token.index<<endl;
             return token;
         }
-        cout<<"GAME OVER"<<endl;
+        ////cout<<"GAME OVER"<<endl;
     }
     strcpy(token.name, "$");
     token.line = line;
     token.type = TokenType::OTHER;
     strcpy(token.value, "$");
     ////token->next = NULL;
-    cout << "OUT" << endl;
+
+    ////cout << "OUT" << endl;
     return token;
 }
 
@@ -434,7 +477,7 @@ void scanner() {
     }
 
     //!
-    cout<<"CANNOT REACH HERE."<<endl;
+    ////cout<<"CANNOT REACH HERE."<<endl;
 
 
 	/*for (int i = 0; i<tokens.size(); ++i) {
@@ -463,7 +506,14 @@ void printTokenLink() {
     
     for(auto token : tokens) {
         string s(token.name);
-        cout <<token.name<<"    "<<"<"<<TokenDict[token.type]<<", "<< seekKey(s) <<">"<<endl;
+        if(seekKey(s)<30) 
+            cout <<token.name<<"    "<<"<"<<TokenDict[token.type]<<", "<< seekKey(s) <<">" << "     " << "in line: "<<token.line<<endl;
+        else if(seekKey(s)>50 && seekKey(s)<70)
+            cout <<token.name<<"    "<<"<"<<TokenDict[token.type]<<", "<< seekKey(s)-50 <<">"<< "      "<< "in line: "<<token.line<<endl;
+        else if(seekKey(s)>80 && seekKey(s)<90)
+            cout <<token.name<<"    "<<"<"<<TokenDict[token.type]<<", "<< seekKey(s)-80 <<">"<< "      "<< "in line: "<<token.line<<endl;
+        else 
+            cout <<token.name<<"    "<<"<"<<TokenDict[token.type]<<", "<< token.value <<">"<< "     "<< "in line: "<<token.line<<endl;
     }
 
 }
